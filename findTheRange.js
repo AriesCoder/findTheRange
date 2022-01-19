21e63d670a3eae731f6c3083b4fe2b26f83de0b0
Trang Lam <ms.tranglam@gmail.com>
2022-01-18 19:52:32 -0800

    gets it to work

--- findTheRange.js
+++ findTheRange.js
@@ -1,4 +1,16 @@
 // Starting array
 let array = [28, 43, -12, 30, 4, 0, -36]
+let smallestNum = array[0]
+let largestNum = array[0]
+for(i = 1; i < array.length; i++){
+    if(array[i]<smallestNum){
+        smallestNum = array[i]
+    }
+    if(array[i]>largestNum){
+        largestNum = array[i]
+    }
+}
 
+console.log("The smallest number is: ", smallestNum)
+console.log("The largest number is: ",largestNum)
 // Write your solution below:
